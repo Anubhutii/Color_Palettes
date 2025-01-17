bc69debe5b18da6f46cf30ba91948028cd687325
Abhishek Shakya <142906871+Abhi125stack@users.noreply.github.com>
2025-01-18 02:46:02 +0530

    Revert "added image picker"

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import ExplorePalette from './Components/ExplorePalette';
 import Visualize from './Components/visualize';
 import 'antd/dist/reset.css';
 import Categories from './Components/Categories';
-import ImagePicker from './Components/ImagePicker';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -36,7 +35,6 @@ function App() {
         <Route path="/explore" element={<ExplorePalette />} />
         <Route path="/categories" element={<Categories/>}/>
         <Route path="/visualize" element={<Visualize />} />
-        <Route path="/image_picker" element={<ImagePicker />}/>
       </Routes>
       {/* Render Footer only if not on the '/generator' or '/explore' routes */}
       {location.pathname === '/' &&   <Footer />}
